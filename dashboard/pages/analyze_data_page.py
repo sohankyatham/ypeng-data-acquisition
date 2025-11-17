606384bc57d5288e89ab64a1993404f79fd2f068
sohankyatham <Sohan.Kyatham23@gmail.com>
2025-11-17 12:07:31 -0500

    Upload file feature to analye_data_page.py

--- dashboard/pages/analyze_data_page.py
+++ dashboard/pages/analyze_data_page.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Analyze Data Page")
 st.write("Hello World! This is the Analyze Data page.")
+
+st.write("Use the tools below to upload your data, preview it, and perform basic analysis.")
+
+# Upload
+st.markdown("### 🔼 Upload Data File")
+uploaded_file = st.file_uploader("Upload CSV, Excel, or text data files", type=['csv', 'xlsx', 'txt'])
