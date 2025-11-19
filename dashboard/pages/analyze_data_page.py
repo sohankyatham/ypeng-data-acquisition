9b7e86a476325cef3cfdb8e62aed2bd92d71fa36
sohankyatham <Sohan.Kyatham23@gmail.com>
2025-11-19 00:07:21 -0500

    Cleaned up file for deployment

--- dashboard/pages/analyze_data_page.py
+++ dashboard/pages/analyze_data_page.py
@@ -6,9 +6,6 @@ import streamlit as st
 import pandas as pd
 
 st.title("Analyze Data Page")
-st.write("Hello World! This is the Analyze Data page.")
-
-st.write("Use the tools below to upload your data, preview it, and perform basic analysis.")
 
 # Upload
 st.markdown("### 🔼 Upload Data File")
